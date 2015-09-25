10dbfa341d9033b761abbc72de441ab639967eb3
Wendy Brannon <wb9753@gmail.com>
2015-09-25 06:51:03 -0600

    committing work done during the workshop

---
+++ du/actions/catapult.rb
@@ -0,0 +1,44 @@
+require 'bundler/setup'
+require 'dino'
+
+class Catapult
+
+  def initialize
+    @board = Dino::Board.new(Dino::TxRx::Serial.new)
+    @button = Dino::Components::Button.new(pin: 6, board: @board)
+    @button.up do
+      fire
+    end
+    @led = Dino::Components::Led.new(pin: 12, board: @board)
+    @servo = Dino::Components::Servo.new(pin: 10, board: @board, position: 90)
+  end
+
+  def load
+    puts 'loading'
+    @servo.position = 1
+  end
+
+  def fire
+    countdown(rand(5))
+    @servo.position = 90
+  end
+
+  def countdown(seconds)
+    signal
+    system("afplay alert.mp3")
+    system("afplay launch.mp3")
+  end
+
+  def signal
+    [:on, :off].cycle(4) do |switch|
+      @led.send(switch)
+      sleep 0.5
+    end
+  end
+end
+__END__
+
+irb -r'./catapult.rb'
+catapult = Catapult.new
+spoon down?
+catapult.load
---
+++ du/actions/servo.rb
@@ -0,0 +1,12 @@
+require 'bundler/setup'
+require 'dino'
+require 'byebug'
+
+board = Dino::Board.new(Dino::TxRx::Serial.new)
+servo = Dino::Components::Servo.new(pin: 10, board: board)
+byebug
+[0, 45, 90].cycle do |angle|
+	servo.position = angle
+	sleep 1
+end
+
